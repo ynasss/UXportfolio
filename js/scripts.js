0a40992929189ac1fa6256aa5e6d94426ad92d6a
ynasss <radoulova.alexandra@gmail.com>
2023-11-24 15:44:44 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function menuToggle () {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle" && $(window).width() < 640) {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "navtoggle"
+    }
+}
+$(document).ready(function() {
+$(".toggle-button").click(function() {
+    var combineId = $(this).data("id");
+    var textButton = $(this).text();
+
+    $("#" + combineId).toggle();
+
+    if (textButton === $(this).data("read-more")) {
+        $(this).text($(this).data("read-less"));
+    }
+    else {
+        $(this).text($(this).data("read-more"));
+    }
+
+
+});
+});
