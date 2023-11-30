558f18765b33bff2df3d304c0e36012a2f32ef5f
ynasss <radoulova.alexandra@gmail.com>
2023-11-30 18:36:49 +0100

    rebuild

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,22 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+var images = document.getElementsByClassName('myImg');
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
+
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
